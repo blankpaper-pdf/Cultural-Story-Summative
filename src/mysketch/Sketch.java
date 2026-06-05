8a2b5874ba136a9200784be9d3e42b560e5a1572
Ivy <>
2026-06-05 11:20:03 -0400

    added

--- src/mysketch/Sketch.java
+++ src/mysketch/Sketch.java
@@ -32,7 +32,8 @@ public class Sketch extends PApplet {
         textSize(15);
         
         a = new Animal(this, 50, 200, "Rabbit", "Spawn", "images/rabbit.png");
-        c1 = new Human(this, 300, 200, "Chang'e", "Spawn", a, "images/chang-e.png");
+        c1 = new Human(this, 300, 250, "Chang'e", "Spawn", a, "images/chang-e.png");
+        c2 = new Human(this, 400, 200, "Houyi", "Spawn", a, "images/houyi.png");
     }
     
     public void draw(){
@@ -44,30 +45,56 @@ public class Sketch extends PApplet {
             background(216, 220, 230);
             image(bg, 0, 0, width, height);
             a.draw();
+            c2.draw();
             c1.draw();
             if (c1.isCollidingWith(a)) {
+                fill(255);
                 this.text("Press 'E' to interact", a.x, a.y);
+                if (keyPressed) {
+                    if (key == 'e' || key == 'E') {
+                        strokeWeight(1);
+                        stroke(255);
+                        fill(0);
+                        rect(0, 300, width-1, 50);
+                        fill(255);
+                        text("[intelligible bunny noises ... what could it be trying to communicate]", 5, 330);
+                    }
+                }
             }
+            if (c1.isCollidingWith(c2)) {
+                fill(255);
+                this.text("Press 'E' to interact", c2.x, c2.y);
+                if (keyPressed) {
+                    if (key == 'e' || key == 'E') {
+                        strokeWeight(1);
+                        stroke(255);
+                        fill(0);
+                        rect(0, 300, width-1, 50);
+                        fill(255);
+                        text("Greetings Chang'e. The Emperor summoned ... [idk]", 5, 330);
+                    }
+                }
+            }
+            fill(255);
             text("Use arrow keys keys to move character", 20, 50);
-            text("Use WASD keys to move rabbit", 20, 80);
         
         if (keyPressed) {
             if (keyCode == LEFT) {
-                c1.move(-5, 0);
+                c1.move(-3, 0);
             } else if (keyCode == RIGHT) {
-                c1.move(5, 0);
+                c1.move(3, 0);
             } else if (keyCode == UP) {
-                c1.move(0, -5);
+                c1.move(0, -3);
             } else if (keyCode == DOWN) {
-                c1.move(0, 5);
-            } else if (key == 'a' || key == 'A') {
-                a.move(-5, 0);
-            } else if (key == 'd' || key == 'D') {
-                a.move(5, 0);
-            } else if (key == 'w' || key == 'W') {
-                a.move(0, -5);
-            } else if (key == 's' || key == 'S') {
-                a.move(0, 5);
+                c1.move(0, 3);
+//            } else if (key == 'a' || key == 'A') {
+//                a.move(-5, 0);
+//            } else if (key == 'd' || key == 'D') {
+//                a.move(5, 0);
+//            } else if (key == 'w' || key == 'W') {
+//                a.move(0, -5);
+//            } else if (key == 's' || key == 'S') {
+//                a.move(0, 5);
             }
         }
         }
